49b9ca4a2ebc07d711ec94d24ece657aa308fcad
Roman <korostinckii.r@phystech.edu>
2021-11-12 16:37:56 +0300

    Made InsertBack

--- include/List.h
+++ include/List.h
@@ -13,34 +13,51 @@ typedef double data_t;
 
 enum ListErrors
 {
-    DATA_NULL_PTR = 1,
-    NEXT_NULL_PTR = 2,
+    ELEM_NULL_PTR = 1,
+    LIST_NULL_PTR = 2,
 };
 
-//typedef struct ListElem
-//{
-//    data_t data = 0;
-//    int next    = 0;
-//    int prev    = 0;
-//}ListElem;
+typedef struct ListElem
+{
+    data_t data = 0;
+    int next    = 0;
+    int prev    = 0;
+}ListElem;
 
 typedef struct List
 {
-//    ListElem* elem = nullptr;
-    data_t* data = nullptr;
-    int*    next = nullptr;
-    int*    prev = nullptr;
+    ListElem* elem = nullptr;
 
     int capacity = 0;
 
     int head = 0;
     int tail = 0;
+    int free = 0;
 }List;
 
-const int LIST_CAPACITY = 1024;
+#define LIST_CHECK(func)  \
+do  \
+{   \
+    if (!list)  \
+    {   \
+        printf("In Function " #func ": data is nullptr");   \
+        return LIST_NULL_PTR;                               \
+    }   \
+    if (list->elem) \
+    {   \
+        printf("In Function " #func ": elem is nullptr");   \
+        return ELEM_NULL_PTR;                               \
+    }   \
+}while (0) \
 
-int ListCtor(List* list, int capacity);
+const int LIST_CAPACITY = 4;
 
-int ListDtor(List* list);
+int ListCtor (List* list, int capacity);
+
+int ListDtor (List* list);
 
 int ListInsertBack (List* list, int value);
+
+int SetMem (List* list);
+
+int SetPoison (List* list);
--- src/ListFunc.cpp
+++ src/ListFunc.cpp
@@ -4,49 +4,108 @@ int ListCtor(List* list, int capacity)
 {
     list->capacity = capacity;
 
-    list->data = (data_t*)calloc(list->capacity, sizeof(data_t));
-    list->next = (int*)   calloc(list->capacity, sizeof(int));
+    list->elem = (ListElem*) calloc(list->capacity, sizeof(ListElem));
 
-    memset(list->next + 1, -1, list->capacity * sizeof(int));
+    SetMem(list);
 
     list->head = 1;
     list->tail = 1;
+    list->free = 1;
 
     return 0;
 }
 
 int ListDtor(List* list)
 {
-    if (!list->data)
-    {
-        printf("In Function ListDtor: data is nullptr");
-        return DATA_NULL_PTR;
-    }
-    if (!list->next)
-    {
-        printf("In Function ListDtor: neat is nullptr");
-        return NEXT_NULL_PTR;
-    }
+    LIST_CHECK(__FUNCTION__);
 
-    memset(list->data, (int)0XBEDABEDA, list->capacity * sizeof(data_t));
-    memset(list->next, (int)0XBEDABEDA, list->capacity * sizeof(int));
+    SetPoison(list);
 
     list->head = -1;
     list->tail = -1;
+    list->free = -1;
 
     return 0;
 }
 
-int ListInsertBack (List* list, int value)
+int ListInsertBack (List* list, data_t value)
 {
-    list->data[list->tail] = value;
 
-    list->tail = 1;
-    while (list->next[list->tail] != -1)
-        list->tail++;
+    LIST_CHECK(__FUNCTION__);
 
-    list->next[list->head] = list->tail;
-    list->next[list->tail] = 0;
+    list->elem[list->free].data = value; //1
+
+    if (list->head == list->free)
+    {
+        list->elem[list->tail].next = 0;
+        list->elem[list->tail].prev = 0;
+
+        list->free = list->elem[list->tail].next;
+    }
+    else
+    {
+        list->elem[list->tail].next = list->free; //2
+
+        list->elem[list->free].prev = list->tail; //3
+
+        list->tail = list->free; //4
+
+        list->free = list->elem[list->tail].next; //5
+
+        list->elem[list->tail].next = 0; //6
+    }
 
     return list->tail;
+}
+
+//int ListDump (List* list)
+//{
+//    LIST_CHECK(__FUNCTION__);
+//
+//    //...
+//}
+//
+//int ListCheck(List* list, char* func)
+//{
+//    if (!list)
+//    {
+//        printf("In Function %s: data is nullptr", func);
+//        return LIST_NULL_PTR;
+//    }
+//    if (list->elem)
+//    {
+//        printf("In Function %s: elem is nullptr", func);
+//        return ELEM_NULL_PTR;
+//    }
+//}
+
+int SetMem(List* list)
+{
+    LIST_CHECK(__FUNCTION__);
+
+    for(int i = 1; i < list->capacity; i++)
+    {
+        if (i < list->capacity - 1)
+            list->elem[i].next = i + 1;
+        else
+            list->elem[i].next = 0;
+
+        list->elem[i].prev = -1;
+    }
+
+    return 0;
+}
+
+int SetPoison (List* list)
+{
+    LIST_CHECK(__FUNCTION__);
+
+    for(int i = 1; i < list->capacity; i++)
+    {
+        list->elem[i].data = (int)0xBEDABEDA;
+        list->elem[i].next = (int)0xBEDABEDA;
+        list->elem[i].prev = (int)0xBEDABEDA;
+    }
+
+    return 0;
 }
